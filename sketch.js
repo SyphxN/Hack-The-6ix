bf9ca77373a47f35279c70c55282b6a441785820
Joel Cherian <joelcherian2005@gmail.com>
2024-08-03 07:41:54 -0400

    Combined midi.js and sketch.js
    Combined the two js scripts and changes playerState on midi input

--- sketch.js
+++ sketch.js
@@ -1,11 +1,75 @@
 gameState ="menu";
 playerState = "idle";
 
+if (navigator.requestMIDIAccess){
+  navigator.requestMIDIAccess().then(success,failure)
+}
+
+function success(midiAccess){
+  console.log(midiAccess);
+  const inputs = midiAccess.inputs;
+  console.log(inputs);
+
+  inputs.forEach((input) => {
+      console.log(input);
+      input.onmidimessage = handleInput;
+  });
+}
+
+function handleInput(input){
+  const noteEvent = input.data[0];
+  const note = input.data[1];
+  const velocity = input.data[2];
+  if(noteEvent == 144){
+    switch(note){
+        case 44:
+            console.log("KICK");
+            playerState = "low";
+            break;
+        case 46:
+            console.log("FLOOR TOM");
+            playerState ="low";
+            break;
+        case 45:
+            console.log("SNARE");
+            playerState = "medium";
+            break;
+        case 49:
+            console.log("HIGH TOM");
+            playerState = "high";
+            break;
+        case 48:
+            console.log("CRASH");
+            playerState="high";
+            break;
+        case 50:
+            console.log("OPEN HI-HAT");
+            playerState ="high";
+            break;
+        case 51:
+            console.log("RIDE");
+            playerState="idle";
+            break;
+        case 47:
+          console.log("CLOSED HI-HAT");
+          playerState = "high";
+          break;
+    }
+  }else{
+    playerState="idle";
+  }
+}
+
+function failure(){
+  console.log("Could not connect to any midi device.")
+}
+
 function setup() {
   // fullscreen canvas
   createCanvas(windowWidth, windowHeight);
   fps = 60;
   frameRate(fps);
+  
 }
 
 function preload() {
@@ -23,30 +87,31 @@ function draw() {
       break;
   }
 }
+
 function menu() {
   background(120);
   textSize(32);
-  text("1 for idle", 10, 30);
-  text("2 for low", 10, 60);
-  text("3 for medium", 10, 90);
-  text("4 for high", 10, 120);
-
+  text("1 for low", 10, 30);
+  text("2 for medium", 10, 60);
+  text("3 for high", 10, 90);
+  /*
   //1 is pressed
   if (keyIsDown(49)){
-    playerState ="idle";
-  }
+    playerState ="low";
+  }else 
   //2 is pressed
   if (keyIsDown(50)){
-    playerState ="low";
-  }
+    playerState ="medium";
+  } else
   //3 is pressed
   if (keyIsDown(51)){
-    playerState ="medium";
-  }
-  //4 is pressed
-  if (keyIsDown(52)){
     playerState ="high";
+  } else 
+  //Default/Idle
+  {
+    playerState ="idle";
   }
+    */
   drawPlayer(playerState, mouseX,mouseY);
 }
 
